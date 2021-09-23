35385ba0893dbc92def73f688a4db16a8e11774e
Mohsin-Siddiqui <56338779+siddiquimohsin@users.noreply.github.com>
2021-09-23 15:20:19 +0500

    New Tests are Added

---
+++ Swerv Tests/RadixSort/RadixSort.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int get_max (int a[], int n){
+   int max = a[0];
+   for (int i = 1; i < n; i++){
+      if (a[i] > max){
+         max = a[i];}
+    }
+   return max;
+}
+void radix_sort (int a[], int n){
+   int bucket[10][10], bucket_cnt[10];
+   int i, j, k, r, NOP = 0, divisor = 1, lar, pass;
+   lar = get_max (a, n);
+   while (lar > 0){
+      NOP++;
+      lar /= 10;
+   }
+   for (pass = 0; pass < NOP; pass++){
+      for (i = 0; i < 10; i++){
+         bucket_cnt[i] = 0;
+      }
+      for (i = 0; i < n; i++){
+         r = (a[i] / divisor) % 10;
+         bucket[r][bucket_cnt[r]] = a[i];
+         bucket_cnt[r] += 1;
+      }
+      i = 0;
+      for (k = 0; k < 10; k++){
+         for (j = 0; j < bucket_cnt[k]; j++){
+            a[i] = bucket[k][j];
+            i++;
+         }
+      }
+      divisor *= 10;
+   }
+}
+void main (){
+   int i, n=10, a[10]={456,223,89,12,678,433,123,900,44,678};
+   
+   radix_sort (a, n);
+   
+}
